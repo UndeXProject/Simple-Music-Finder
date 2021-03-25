23e0d3b28117eeeadb1d3e380f6ddae01f42078e
Und3X <volodin.bi@yandex.ru>
2021-03-25 16:22:28 +0300

    Добавьте файлы проекта.

---
+++ Simple Music Finder/Classes/Provider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace Simple_Music_Finder.Classes
+{
+    abstract class Provider
+    {
+        /// <summary>
+        /// Имя провайдера
+        /// </summary>
+        public abstract string Name { get; internal set; }
+        /// <summary>
+        /// Инициализация плагина
+        /// </summary>
+        public abstract void Init();
+        /// <summary>
+        /// Поиск по тексту
+        /// </summary>
+        /// <param name="Text">Текст для поиска</param>
+        /// <returns></returns>
+        public abstract string FindByText(string Text);
+        /// <summary>
+        /// Данные о плагине
+        /// Обязательные ключи: author, version, url
+        /// </summary>
+        public abstract Dictionary<string, string> ProviderData { get; internal set; }
+        private static List<Provider> ProvidersList = new List<Provider>();
+        public static void Add(Provider provider)
+        {
+            ProvidersList.Add(provider);
+        }
+        public static void Load(Provider provider)
+        {
+            provider.Init();
+        }
+        public static List<Provider> GetList()
+        {
+            return ProvidersList;
+        }
+    }
+
+
+}
---
+++ Simple Music Finder/Classes/Providers/GetTune.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simple_Music_Finder.Classes.Providers
+{
+    class GetTune : Provider
+    {
+        public override string Name { get; internal set; }
+        public override Dictionary<string, string> ProviderData { get; internal set; }
+
+        /// <summary>
+        /// Поиск по текстовому запросу
+        /// </summary>
+        /// <param name="Text">Текст для поиска</param>
+        /// <returns></returns>
+        public override string FindByText(string Text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Init()
+        {
+            Name = "GetTune";
+            ProviderData = new Dictionary<string, string>();
+            ProviderData.Add("author", "Und3X");
+            ProviderData.Add("version", "1.0.0");
+            ProviderData.Add("url", "https://und3x.ru/");
+            Add(this);
+        }
+    }
+}
